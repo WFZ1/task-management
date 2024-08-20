2a26b042aa5b24951c94ee99fe4ed4b614283512
WFZ1 <pavelskumenko@gmail.com>
2024-08-20 08:40:30 +0300

    refactor: unbundling env variables from browser

--- src/utils/supabase/server.ts
+++ src/utils/supabase/server.ts
@@ -4,7 +4,7 @@ import { cookies } from 'next/headers';
 export const createClient = () => {
     const cookieStore = cookies();
 
-    return createServerClient(process.env.NEXT_PUBLIC_SUPABASE_URL!, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!, {
+    return createServerClient(process.env.SUPABASE_URL!, process.env.SUPABASE_ANON_KEY!, {
         cookies: {
             get(name: string) {
                 return cookieStore.get(name)?.value;
